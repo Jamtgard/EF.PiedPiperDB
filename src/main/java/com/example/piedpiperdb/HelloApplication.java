4fcd38deaac89018c9ca08804308038c37d88020
jamtgard <jamtgardsimon@gmail.com>
2024-12-03 11:36:12 +0100

    Initial commit

---
+++ src/main/java/com/example/piedpiperdb/HelloApplication.java
@@ -0,0 +1,24 @@
+package com.example.piedpiperdb;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        AnchorPane anchorPane = new AnchorPane();
+        Scene scene = new Scene(anchorPane, 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module com.example.piedpiperdb {
+    requires javafx.controls;
+
+
+    exports com.example.piedpiperdb;
+}
