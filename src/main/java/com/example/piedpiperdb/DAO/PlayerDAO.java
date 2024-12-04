9a1059e442b8ce32b324187d5daafab00434cdd3
Sommar-skog <amandaaronsson@hotmail.com>
2024-12-04 15:03:50 +0100

    GEFP-12-AA - added savePlayer()

--- src/main/java/com/example/piedpiperdb/DAO/PlayerDAO.java
+++ src/main/java/com/example/piedpiperdb/DAO/PlayerDAO.java
@@ -1,8 +1,38 @@
 package com.example.piedpiperdb.DAO;
 
+import com.example.piedpiperdb.Entities.Player;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
 //GEFP-12-AA
 public class PlayerDAO {
+    //CRUD-opperationer (Create - Read - Update - Delete)
 
+    private final EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence.createEntityManagerFactory("myconfig");
+
+    //Create
+    public boolean savePlayer(Player player) {
+        EntityManager entityManager = ENTITY_MANAGER_FACTORY.createEntityManager();
+        EntityTransaction transaction = null;
+
+        try {
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+            entityManager.persist(player);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            if (entityManager != null && transaction != null && transaction.isActive()) {
+                transaction.rollback();
+            }
+            return false;
+        } finally {
+            entityManager.close();
+        }
+    }
 
 
 }
