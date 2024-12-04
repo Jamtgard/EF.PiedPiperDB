d1492df10948ebb832d3e5507794a12ce34146bc
jamtgard <jamtgardsimon@gmail.com>
2024-12-04 10:40:25 +0100

    Commit #3 - module-info & persistence fixes. (SJ AA SA AWS (Teams))

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,9 @@
 module com.example.piedpiperdb {
     requires javafx.controls;
+    requires jakarta.persistence;
+    requires org.hibernate.orm.core;
 
-
-    exports com.example.piedpiperdb;
+    opens com.example.piedpiperdb.Entities to org.hibernate.orm.core;
+//    exports com.example.piedpiperdb;
     exports com.example.piedpiperdb.View;
 }
