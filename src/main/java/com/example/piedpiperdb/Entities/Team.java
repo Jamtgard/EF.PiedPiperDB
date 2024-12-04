e7fdecc3c1dac59cd4b12791502658f7a20e034e
jamtgard <jamtgardsimon@gmail.com>
2024-12-04 16:17:09 +0100

    Commit #2 - Entity: Team

--- src/main/java/com/example/piedpiperdb/Entities/Team.java
+++ src/main/java/com/example/piedpiperdb/Entities/Team.java
@@ -13,16 +13,15 @@ public class Team {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "team_id")
-    private int id;
+    private int teamId;
 
     @Column(name = "team_name", length = 20, nullable = false, unique = true)
     private String teamName;
 
-    /*
+
     @OneToMany(mappedBy = "playerId", fetch = FetchType.EAGER, cascade = CascadeType.PERSIST, orphanRemoval = true)
     @JoinColumn (name = "players", nullable = false)
     private List<Player> listOfPlayersInTeam = new ArrayList<>();
-     */
 
     // Constructors
 
@@ -34,24 +33,14 @@ public class Team {
 
     // Getters & Setters
 
-    public int getId() {return id;}
-    public void setId(int id) {this.id = id;}
+    public int getId() {return teamId;}
+    public void setId(int id) {this.teamId = teamId;}
 
     public String getTeamName() {return teamName;}
     public void setTeamName(String teamName) {this.teamName = teamName;}
 
-    /*
     public List<Player> getListOfPlayersInTeam() {return listOfPlayersInTeam;}
     public void setListOfPlayersInTeam(List<Player> listOfPlayersInTeam) {this.listOfPlayersInTeam = listOfPlayersInTeam;}
 
-    public Game getGameId() {return gameId;}
-    public void setGameId(Game gameId) {this.gameId = gameId;}
-
-    public Team getTeamId() {return teamId;}
-    public void setTeamId(Team teamId) {this.teamId = teamId;}
-
-    public Match getMatchId() {return matchId;}
-    public void setMatchId(Match matchId) {this.matchId = matchId;}
-     */
 }
 
