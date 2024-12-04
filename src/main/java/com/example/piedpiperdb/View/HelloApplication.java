fad4ae9b4861b8b79fb253116f11de110c259c96
Sara <sara.andersson185@gmail.com>
2024-12-04 14:20:23 +0100

    GEFP-5-SA: Börjat på startsidan

--- src/main/java/com/example/piedpiperdb/View/HelloApplication.java
+++ src/main/java/com/example/piedpiperdb/View/HelloApplication.java
@@ -2,18 +2,35 @@ package com.example.piedpiperdb.View;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+    Stage window;
+    Scene startScene;
+
+    private Button loginButton;
+
+    private final Label programTitel = new Label("Hello World");
+
+
     @Override
     public void start(Stage stage) throws IOException {
 
-        AnchorPane anchorPane = new AnchorPane();
-        Scene scene = new Scene(anchorPane, 320, 240);
+        //AnchorPane anchorPane = new AnchorPane();
+        StackPane stackPane = new StackPane();
+
         stage.setTitle("Hello!");
+
+
+
+        Scene scene = new Scene(stackPane, 320, 240);
         stage.setScene(scene);
         stage.show();
     }
