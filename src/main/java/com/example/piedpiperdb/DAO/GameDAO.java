df3bb8c5c7e882d88cea49901d5e0c0302ee7a49
Sara <sara.andersson185@gmail.com>
2024-12-04 14:10:42 +0100

    GEFP-6-SA: La till GameDAO

---
+++ src/main/java/com/example/piedpiperdb/DAO/GameDAO.java
@@ -0,0 +1,112 @@
+package com.example.piedpiperdb.DAO;
+
+import jakarta.persistence.*;
+import com.example.piedpiperdb.Entities.Game;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameDAO {
+
+    private static final EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence.createEntityManagerFactory("myconfig");
+
+    //Create
+    public boolean saveGame(Game game){
+        EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
+        EntityTransaction tx = null;
+        try{
+            tx = em.getTransaction();
+            tx.begin();
+            em.persist(game);
+            tx.commit();
+            return true;
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            if(em != null && tx != null && tx.isActive()){
+                tx.rollback();
+            }
+            return false;
+        }
+    }
+
+    //Read
+    public Game getGameById(int id){
+        EntityManager entityManager = ENTITY_MANAGER_FACTORY.createEntityManager();
+        EntityTransaction transaction = null;
+        try{
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+            Game gameToReturn = entityManager.find(Game.class, id);
+            transaction.commit();
+            return gameToReturn;
+        }catch(Exception e){
+            if(entityManager != null && transaction != null && transaction.isActive()){
+                System.out.println(e.getMessage());
+            }
+            return null;
+        }finally {
+            entityManager.close();
+        }
+    }
+
+    public List<Game> getAllGames(){
+        EntityManager entityManager = ENTITY_MANAGER_FACTORY.createEntityManager();
+        List<Game> listToReturn = new ArrayList<>();
+        TypedQuery<Game> result = entityManager.createQuery("FROM Game", Game.class);
+        listToReturn.addAll(result.getResultList());
+        return listToReturn;
+    }
+
+    //Update
+    public void updateGame(Game gameToUpdate){
+        //carToUpdate kommer in
+        EntityManager entityManager = ENTITY_MANAGER_FACTORY.createEntityManager();
+        EntityTransaction transaction = null;
+        try {
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+            if(entityManager.contains(gameToUpdate)){
+                System.out.println("Spelet finns i poolen");
+                entityManager.persist(gameToUpdate);//Helt nya saker som ska sparas
+
+            } else {
+                System.out.println("Finns inte i poolen");
+                Game revivedGame = entityManager.merge(gameToUpdate);//merge = hämtar från poolen
+                System.out.println(revivedGame.getId() + " is alive");
+            }
+            entityManager.merge(gameToUpdate);//För att uppdatera en bil som redan ska finnas i db
+            transaction.commit();
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+            if(entityManager != null && transaction != null && transaction.isActive()){
+                transaction.rollback();
+            }
+        } finally {
+            entityManager.close();
+        }
+    }
+
+    //Delete
+    public boolean deleteGameById(int id){
+        EntityManager entityManager = ENTITY_MANAGER_FACTORY.createEntityManager();
+        EntityTransaction transaction = null;
+        try {
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+            Game gameToDelete = entityManager.find(Game.class, id);
+            entityManager.remove(entityManager.contains(gameToDelete) ? gameToDelete : entityManager.merge(gameToDelete));
+            transaction.commit();
+            return true;
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            if(entityManager != null && transaction != null && transaction.isActive()){
+                transaction.rollback();
+            }
+            return false;
+        }
+        finally {
+            entityManager.close();
+        }
+    }
+
+}
