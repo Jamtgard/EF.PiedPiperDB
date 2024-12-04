c653225ac113e3836c6ce562bf1898efa171044d
Sara <sara.andersson185@gmail.com>
2024-12-04 13:50:38 +0100

    GEFP-7-SA: La till Game enteties klassen

---
+++ src/main/java/com/example/piedpiperdb/Entities/Game.java
@@ -0,0 +1,67 @@
+package com.example.piedpiperdb.Entities;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "games")
+public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "game_id")
+    private int game_id;
+
+    @Column(name = "game_name",length = 40,nullable = false)
+    private String game_name;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "match_id")
+    private Match match_id;
+
+    @OneToMany(mappedBy = "player", orphanRemoval = true,fetch = FetchType.EAGER,cascade = CascadeType.PERSIST)
+    @JoinColumn
+    private List<Player> players = new ArrayList<>();
+
+    @OneToMany(mappedBy = "team", orphanRemoval = true,fetch = FetchType.EAGER,cascade = CascadeType.PERSIST)
+    @JoinColumn
+    private List<Team> teams = new ArrayList<>();
+
+    /*
+    @OneToMany(mappedBy = "", orphanRemoval = true,fetch = FetchType.EAGER,cascade = CascadeType.PERSIST)
+    @JoinColumn
+    private List<Match> matches = new ArrayList<>();
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "team_id")
+    private Team team_id;*/
+
+    public Game() {
+
+    }
+
+    public Game(String game_name) {
+        this.game_name = game_name;
+    }
+
+    public Match getMatch_id() {return match_id;}
+
+    public void setMatch_id(Match match_id) {this.match_id = match_id;}
+
+    public List<Player> getPlayers() {return players;}
+
+    public void setPlayers(List<Player> players) {this.players = players;}
+
+    public List<Team> getTeams() {return teams;}
+
+    public void setTeams(List<Team> teams) {this.teams = teams;}
+
+    public String getGame_name() {return game_name;}
+
+    public void setGame_name(String game_name) {this.game_name = game_name;}
+
+    public int getGame_id() {return game_id;}
+
+    public void setGame_id(int game_id) {this.game_id = game_id;}
+}
