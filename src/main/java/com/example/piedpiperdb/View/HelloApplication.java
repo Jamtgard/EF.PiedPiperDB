b40ed5bcfaef57c19ea371f23c8bd4a51c2679a5
Sara <sara.andersson185@gmail.com>
2024-12-04 15:20:10 +0100

    GEFP-5-SA: Ändrat med färger mm

--- src/main/java/com/example/piedpiperdb/View/HelloApplication.java
+++ src/main/java/com/example/piedpiperdb/View/HelloApplication.java
@@ -1,11 +1,13 @@
 package com.example.piedpiperdb.View;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -17,21 +19,48 @@ public class HelloApplication extends Application {
 
     private Button loginButton;
 
+    private final int height = 600;
+    private final int width = 700;
+
     private final Label programTitel = new Label("Hello World");
 
 
     @Override
     public void start(Stage stage) throws IOException {
 
+        window = stage;
+        window.setResizable(false);
         //AnchorPane anchorPane = new AnchorPane();
         StackPane stackPane = new StackPane();
 
-        stage.setTitle("Hello!");
+        stage.setTitle("Start page");
+
+
+        Rectangle greenBackground = new Rectangle(width, height);
+        greenBackground.setHeight(400);
+        greenBackground.setWidth(500);
+        greenBackground.setFill(Paint.valueOf("#D0E3C4"));
+
+        programTitel.getStyleClass().add("titel");
+        loginButton = new Button("Login");
+        loginButton.getStyleClass().add("standardButton");
+
+
+        VBox vBox = new VBox();
+        vBox.setSpacing(10);
+        vBox.setAlignment(Pos.CENTER);
+        vBox.getStyleClass().add("backgroundOlivine");
+
+        vBox.getChildren().addAll(programTitel, loginButton);
+
+
+        stackPane.getChildren().addAll(greenBackground, vBox);
 
 
 
-        Scene scene = new Scene(stackPane, 320, 240);
-        stage.setScene(scene);
+        startScene = new Scene(stackPane, width, height);
+        startScene.getStylesheets().add("EscortFlasher.css");
+        stage.setScene(startScene);
         stage.show();
     }
 
