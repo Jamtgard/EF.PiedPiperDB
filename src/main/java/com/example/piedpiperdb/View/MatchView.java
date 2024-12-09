184c7f145b5ed8e61f04a849c3e8afd997d11c98
a-westerberg <alexander.westerberg.svedberg@gmail.com>
2024-12-09 10:53:46 +0100

    GEFP-23-aws  små kod

--- src/main/java/com/example/piedpiperdb/View/MatchView.java
+++ src/main/java/com/example/piedpiperdb/View/MatchView.java
@@ -1,4 +1,30 @@
 package com.example.piedpiperdb.View;
 
-public class MatchView {
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class MatchView extends AbstractScene {
+
+    private static ListView matchListView;
+    private static Button submit;
+
+    public static Scene startSceneMatch(Stage window){
+        Scene baseScene = AbstractScene.getScene(window);
+        AnchorPane anchorPane = AbstractScene.anchorPane;
+        VBox vBox = AbstractScene.leftVbox;
+
+        AbstractScene.back.setOnAction(event -> {
+
+        });
+
+
+        return baseScene;
+    }
+
+
+
 }
