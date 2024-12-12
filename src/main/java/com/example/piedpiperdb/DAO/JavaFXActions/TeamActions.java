b2525886d9996199af0c1ddf2b6a7215127b06f7
jamtgard <jamtgardsimon@gmail.com>
2024-12-12 10:23:41 +0100

    GEFP-21-SJ - TeamView (Basics complete) + AlertBox (Optimized: Now the Stage is able to scale based on message input - allowing for a more dynamic usage of class)

---
+++ src/main/java/com/example/piedpiperdb/DAO/JavaFXActions/TeamActions.java
@@ -0,0 +1,13 @@
+package com.example.piedpiperdb.DAO.JavaFXActions;
+
+import com.example.piedpiperdb.DAO.TeamDAO;
+import com.example.piedpiperdb.Entities.Team;
+
+//GEFP-27-SJ
+public class TeamActions {
+
+    private static TeamDAO teamDAO = new TeamDAO();
+
+   // public static
+
+}
