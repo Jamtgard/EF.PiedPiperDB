5de13e2651a9da76844569dbf9cb234531c66e94
AnnaRunnander21 <anna_runnander@hotmail.com>
2024-12-23 15:53:45 +0100

    Add files via upload

--- StaffDAO.java
+++ StaffDAO.java
@@ -8,6 +8,7 @@ import com.example.piedpiperdb.Entities.Game;
 import java.util.ArrayList;
 import com.example.piedpiperdb.Entities.Player;
 
+//GEFP-45 Anna R
 public class StaffDAO {
     private static final EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence.createEntityManagerFactory("piedpiperdb");
 
@@ -52,4 +53,65 @@ public class StaffDAO {
         return "admin".equalsIgnoreCase(staffName);
     }
     private com.example.piedpiperdb.DAO.TeamDAO teamDAO = new com.example.piedpiperdb.DAO.TeamDAO();
+
+    //uppdatera personal
+    public boolean updatePersonal (int staffId, String firstName, String lastName, String nickname, String streetAdress, String city, String postalCode, String country, String email) {
+        EntityManager entityManager = ENTITY_MANAGER_FACTORY.createEntityManager();
+        EntityTransaction transaction = null;
+
+        try {
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+            Staff staff = entityManager.find(Staff.class, staffId);
+            if (staff != null) {
+                staff.setFirstName(firstName);
+                staff.setLastName(lastName);
+                staff.setNickname(nickname);
+                staff.setStreetAdress(streetAdress);
+                staff.setCity(city);
+                staff.setCountry(country);
+                staff.setPostalCode(postalCode);
+                staff.setEmail(email);
+                entityManager.merge(staff);
+                transaction.commit();
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            System.out.println("Error");
+            if (transaction != null && transaction.isActive()) {
+                transaction.rollback();
+            }
+            return false;
+        } finally {
+            entityManager.close();
+        }
+    }
+    //ta bort personal
+    public boolean deletePersonal(int staffId) {
+        EntityManager entityManager = ENTITY_MANAGER_FACTORY.createEntityManager();
+        EntityTransaction transaction = null;
+
+        try {
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+            Staff staff = entityManager.find(Staff.class, staffId);
+            if (staff != null) {
+                entityManager.remove(staff);
+                transaction.commit();
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            System.out.println("Error");
+            if (transaction != null && transaction.isActive()) {
+                transaction.rollback();
+            }
+            return false;
+        } finally {
+            entityManager.close();
+        }
+    }
 }
